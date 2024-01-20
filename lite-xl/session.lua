8a35f7ec90f6dd26ff24216039afff1cfc7cb3e7
Akash <akashisgreat0000@gmail.com>
2024-01-20 21:45:26 +0530

    appliction added to /bin/ and xfce4-clipman autostartup

---
+++ lite-xl/session.lua
@@ -0,0 +1,1 @@
+return {recents={}, window={[1]=1536,[2]=864,[3]=197,[4]=137,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
---
+++ nvim/init.vim
@@ -0,0 +1,5 @@
+set clipboard+=unnamedplus
+set relativenumber
+set wrap           " Enable word wrap
+set whichwrap+=>,l
+set whichwrap+=<,h
